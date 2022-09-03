0e4b0c2e406054e319e41a9eb977c262b76d8d39
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2022-09-03 12:10:26 +0300

    Test that parse and unsafeParse return the same issues

--- __tests__/helpers.ts
+++ __tests__/helpers.ts
@@ -1,4 +1,4 @@
-import { Codec } from "../src/index.js"
+import { Codec, ParseError } from "../src/index.js"
 
 export function expectParseSuccess(
   codec: Codec<unknown>,
@@ -15,5 +15,18 @@ export function expectParseFailure(codec: Codec<unknown>, value: unknown) {
   const result = codec.parse(value)
 
   expect(result).toEqual({ ok: false, issues: expect.any(Array) })
-  if (!result.ok) expect(result.issues).toMatchSnapshot()
+  if (!result.ok) {
+    expect(result.issues).toMatchSnapshot()
+
+    try {
+      codec.unsafeParse(value)
+      throw new Error("Bug: unsafeParse didn't throw an error!")
+    } catch (err) {
+      expect(err).toBeInstanceOf(ParseError)
+      if (err instanceof ParseError) {
+        expect(err.message).toEqual(result.issues[0].message)
+        expect(err.issues).toEqual(result.issues)
+      }
+    }
+  }
 }
