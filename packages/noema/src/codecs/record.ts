17d1be5c34dd943043bea547af0abcb8b539a7ad
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2022-10-22 17:47:29 +0300

    Disable prettier caching
    It doesn't seem to work well, for whatever reason.

--- packages/noema/src/codecs/record.ts
+++ packages/noema/src/codecs/record.ts
@@ -44,7 +44,8 @@ export function record<K extends AnySimpleCodec, V extends AnyCodec>(
 
           const result = values.validate(val[key], ctx)
           if (!result.ok) ok = false
-          else if (!simple && ok && result.value !== undefined) object[key] = result.value
+          else if (!simple && ok && result.value !== undefined)
+            object[key] = result.value
         }
       }
 
