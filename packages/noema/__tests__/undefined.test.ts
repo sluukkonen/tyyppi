1cb15adc8e29af6d0925e6790bb5ecc6731cc20c
Joona Piirainen <joona.piirainen@gmail.com>
2022-11-07 18:02:02 +0200

    feat: add undefined codec
    Also added basic unit tests for the new codec.

---
+++ packages/noema/__tests__/undefined.test.ts
@@ -0,0 +1,23 @@
+import { expectParseFailure, expectParseSuccess } from "./helpers.js"
+import * as n from "../src/index.js"
+
+describe("undefined", () => {
+  test("should parse undefined", () => {
+    expectParseSuccess(n.undefined, undefined)
+  })
+
+  test.each([
+    [null, "invalid_undefined" as const],
+    [0, "invalid_undefined" as const],
+    ["noema", "invalid_undefined" as const],
+    [{}, "invalid_undefined" as const],
+    [[], "invalid_undefined" as const],
+    [true, "invalid_undefined" as const],
+    [false, "invalid_undefined" as const],
+    [() => 1, "invalid_undefined" as const],
+  ])("should not parse other values", (actual, code) => {
+    expectParseFailure(n.undefined, actual, [
+      { code: code, actual: actual, path: [] },
+    ])
+  })
+})
---
+++ packages/noema/src/codecs/undefined.ts
@@ -0,0 +1,27 @@
+import { createSimpleCodec, SimpleCodec } from "../Codec.js"
+import { InvalidUndefined } from "../DecodeError.js"
+import { UndefinedMetadata } from "../Metadata.js"
+import { failure, success } from "../Result.js"
+
+type UndefinedCodec = SimpleCodec<
+  undefined,
+  InvalidUndefined,
+  UndefinedMetadata
+>
+
+const undefinedCodec: UndefinedCodec = /* @__PURE__ */ createSimpleCodec(
+  (val) =>
+    val === undefined
+      ? success(undefined)
+      : failure({
+          code: "invalid_undefined",
+          actual: val,
+          path: [],
+        }),
+  {
+    tag: "undefined",
+    simple: true,
+  }
+)
+
+export { undefinedCodec as undefined }
