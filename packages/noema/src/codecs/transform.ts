f9c421ae2ff1e4870ee1c4ed5372df503bfb90b2
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2023-03-05 20:49:53 +0200

    Remove simpleTransform code
    It is unsound, transforms are never simple

--- packages/noema/src/codecs/transform.ts
+++ packages/noema/src/codecs/transform.ts
@@ -8,13 +8,12 @@ import {
   TypeOf,
 } from "../Codec.js"
 import { Result, success } from "../Result.js"
-import { identity } from "../utils.js"
 
 export interface TransformCodec<C extends AnyCodec, T>
   extends Codec<InputOf<C>, T, ErrorOf<C>> {
   readonly metadata: {
     readonly tag: "transform"
-    readonly simple: boolean
+    readonly simple: false
     readonly codec: C
   }
 }
@@ -24,13 +23,12 @@ export const transform = <C extends AnyCodec, T>(
   forwards: (value: TypeOf<C>) => T,
   backwards: (value: T) => TypeOf<C>
 ): TransformCodec<C, T> => {
-  const simple = codec.metadata.simple && backwards === identity
   return createCodec(
     (val): Result<T, ErrorOf<C>> => {
       const result = codec.decode(val) as ResultOf<C>
       return result.ok ? success(forwards(result.value)) : result
     },
-    simple ? identity : (val) => codec.encode(backwards(val)),
-    { tag: "transform", simple, codec }
+    (val) => codec.encode(backwards(val)),
+    { tag: "transform", simple: false, codec }
   )
 }
