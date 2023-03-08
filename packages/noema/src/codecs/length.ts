15aae6c6e451698b5d4ab3134e359dc71d0dd123
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2023-03-08 21:16:28 +0200

    Do not define length in terms on minLength and maxLength
    I forgot about the issue with error messages not containing both ends of
    the length limits.

--- packages/noema/src/codecs/length.ts
+++ packages/noema/src/codecs/length.ts
@@ -1,13 +1,52 @@
-import { Codec } from "../Codec.js"
+import {
+  Codec,
+  createCodec,
+  ErrorOf,
+  InputOf,
+  IsSimple,
+  Metadata,
+  ResultOf,
+  TypeOf,
+} from "../Codec.js"
+import { tooLong, TooLong, tooShort, TooShort } from "../DecodeError.js"
+import { failure, Result } from "../Result.js"
 import { HasLength } from "../types.js"
-import { maxLength, MaxLengthCodec } from "./maxLength.js"
-import { minLength, MinLengthCodec } from "./minLength.js"
+import { hasLength } from "../utils.js"
 
-export type LengthCodec<C extends Codec<HasLength, any>> = MinLengthCodec<
-  MaxLengthCodec<C>
+interface LengthMetadata<C extends Codec<HasLength, any>> extends Metadata {
+  readonly tag: "length"
+  readonly simple: IsSimple<C>
+  readonly length: number
+  readonly codec: C
+}
+
+export type LengthCodec<C extends Codec<HasLength, any>> = Codec<
+  InputOf<C>,
+  TypeOf<C>,
+  ErrorOf<C> | TooShort | TooLong,
+  LengthMetadata<C>
 >
 
 export const length = <C extends Codec<HasLength, any>>(
   codec: C,
   length: number
-) => minLength(maxLength(codec, length), length)
+): LengthCodec<C> =>
+  createCodec(
+    (val): Result<TypeOf<C>, ErrorOf<C> | TooShort | TooLong> => {
+      if (hasLength(val)) {
+        if (val.length < length)
+          return failure(tooShort(val.length, length, length))
+        if (val.length > length)
+          return failure(tooLong(val.length, length, length))
+      }
+
+      return codec.decode(val) as ResultOf<C>
+    },
+    codec.encode as (value: TypeOf<C>) => InputOf<C>,
+    {
+      tag: "length",
+      simple: codec.meta.simple,
+      codec,
+      length,
+    }
+  )
