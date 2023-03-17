e74b20a96d681c973ff9aac4da150023eb21d22c
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2023-03-17 20:53:20 +0200

    Implmement clamp in terms of min/max

--- packages/noema/src/codecs/clamp.ts
+++ packages/noema/src/codecs/clamp.ts
@@ -1,59 +1,13 @@
-import {
-  Codec,
-  createCodec,
-  ErrorOf,
-  InputOf,
-  IsSimple,
-  Metadata,
-  ResultOf,
-  TypeOf,
-} from "../Codec.js"
-import { tooLarge, TooLarge, tooSmall, TooSmall } from "../DecodeError.js"
-import { failure, Result } from "../Result.js"
+import { Codec, TypeOf } from "../Codec.js"
 import { Ordered } from "../types.js"
+import { MaxCodec, max as maxCodec } from "./max.js"
+import { MinCodec, min as minCodec } from "./min.js"
 
-interface ClampMetadata<C extends Codec<any, Ordered>> extends Metadata {
-  readonly tag: "clamp"
-  readonly simple: IsSimple<C>
-  readonly min: TypeOf<C>
-  readonly max: TypeOf<C>
-  readonly codec: C
-}
-
-export type ClampCodec<C extends Codec<any, Ordered>> = Codec<
-  InputOf<C>,
-  TypeOf<C>,
-  ErrorOf<C> | TooSmall<TypeOf<C>> | TooLarge<TypeOf<C>>,
-  ClampMetadata<C>
->
+export type ClampCodec<C extends Codec<any, Ordered>> = MinCodec<MaxCodec<C>>
 
 export const clamp = <C extends Codec<any, Ordered>>(
   codec: C,
   min: TypeOf<C>,
   max: TypeOf<C>
 ): ClampCodec<C> =>
-  createCodec(
-    (
-      val
-    ): Result<
-      TypeOf<C>,
-      ErrorOf<C> | TooSmall<TypeOf<C>> | TooLarge<TypeOf<C>>
-    > => {
-      const result = codec.decode(val) as ResultOf<C>
-      return result.ok
-        ? result.value < min
-          ? failure(tooSmall(result.value, min))
-          : result.value > max
-          ? failure(tooLarge(result.value, max))
-          : result
-        : result
-    },
-    codec.encode,
-    {
-      tag: "clamp",
-      simple: codec.meta.simple,
-      codec,
-      min,
-      max,
-    }
-  )
+  minCodec(maxCodec(codec, max), min as unknown as TypeOf<MaxCodec<C>>)
